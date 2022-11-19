8c354c4cc567f22a917f785ddc3a437691165510
YanagawaSachiho <yuki-1huji2miso3sinji@docomo.ne.jp>
2022-11-19 22:21:46 +0900

    ユーザー管理ページ

--- resources/views/display/users.blade.php
+++ resources/views/display/users.blade.php
@@ -1,6 +1,15 @@
-@foreach($all_user as $all_user)
-{{$all_user['name']}}
-{{$all_user['email']}}
-{{$all_user['password']}}
-{{$all_user['plofile']}}
-@endforeach
+@extends('layouts.layout')
+@section('content')
+<table class="table">
+  <ul>
+  @foreach($all_user as $all_user)
+  <a href="">
+ <li>ユーザー名 {{$all_user['name']}}</li>
+ <li>メールアドレス{{$all_user['email']}}</li>
+ <li>プロフィール{{$all_user['plofile']}}</li>
+</a>
+<a href=""> <buttum class="btn-warning">削除</buttum></a>
+  @endforeach
+</ul>
+</table>
+@endsection
